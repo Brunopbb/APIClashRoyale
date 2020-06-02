417f78da4db15494938df598beee2fd9cb73bd37
Bruno Almeida <almeidabruno99@gmail.com>
2020-06-02 02:24:03 -0300

    Atualização

--- ClashAPI.py
+++ ClashAPI.py
@@ -111,8 +111,8 @@ class dataProcessing(object):
 
 response = Request()
 
-#current = response.getCurrentWarStatus()["state"]
-current = "warDay"
+current = response.getCurrentWarStatus()["state"]
+
 control = 0
 
 file = open("stateWar.txt", "r")
