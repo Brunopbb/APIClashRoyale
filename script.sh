0b85475d57b0392d5f46a156304a303e22381f3f
Bruno Almeida <almeidabruno99@gmail.com>
2020-07-05 23:06:30 -0300

    Atualização

--- script.sh
+++ script.sh
@@ -4,8 +4,8 @@ python3 /home/bruno/Documentos/clash/APIClashRoyale/ClashAPI.py
 
 git add .
 
-git commit -m "Atualização" >> /home/bruno/Documentos/clash/APIClashRoyale/run.log
+git commit -m "Atualização"
 
-git push origin master >> /home/bruno/Documentos/clash/APIClashRoyale/run.log
+git push origin master 
 
 
